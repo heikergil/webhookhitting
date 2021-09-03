1b4ae45c126262b07b233d71d17dcf673d52b744
Heiker Gil <gilheyker@gmail.com>
2021-09-03 14:47:27 -0500

    finished

--- data.js
+++ data.js
@@ -1,14 +1,11 @@
 const Cambio = require('./models/cambios');
-const mongoose = require('mongoose');
+const connectDB = require('./config/db');
+
+connectDB();
 
 
-mongoose.connect('mongodb://localhost:27017/test', {useNewUrlParser: true, useUnifiedTopology: true});
 
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-  // we're connected!
-});
+
 
 function fechaUTC(dias=0) {
     let fecha = new Date();
--- index.js
+++ index.js
@@ -1,75 +1,33 @@
 const express = require('express')
 const app = express();
 const mongoose = require('mongoose');
-const Cambio = require('./models/cambios');
 const axios = require('axios');
 const engine = require("ejs-mate");
 const path = require("path");
+const connectDB = require('./config/db');
+const Cambio = require('./models/cambios');
 
 
-function fechaUTC(dias=0) {
-  let fecha = new Date();
-  fecha.setUTCDate(fecha.getUTCDate() - dias);
-  return fecha
-}
 
-
-mongoose.connect('mongodb://localhost:27017/test', {useNewUrlParser: true, useUnifiedTopology: true});
-
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-  // we're connected!
-});
-
-
-app.set("views", path.join(__dirname, "views"));
-app.engine("ejs", engine);
-app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 
-    const datosCambio = [
-        {moneda: 'EUR',
-        valor: 2.4,
-        fecha:fechaUTC()},
-        {moneda: 'EUR',
-        valor: 2.3,
-        fecha:fechaUTC(1)},
-        {moneda: 'EUR',
-        valor: 2.4,
-        fecha:fechaUTC(2)},
-        {moneda: 'EUR',
-        valor: 2.5,
-        fecha:fechaUTC(3)},
-        {moneda: 'EUR',
-        valor: 2.6,
-        fecha:fechaUTC(4)},
-      ]
-
+  
+connectDB();
 
 
 app.get('/consulta', async (req, res) => {
 
-  const datos = req.query
-    // const data = JSON.stringify(datosCambio);
-    
-    // axios
-    //     .post('https://webhook.site/fe1b3323-f455-429a-af7a-6ffb74e757ef', {
-    //         headers: {
-    //             'Content-Type': 'application/json; charset=UTF-8',
-    //           },
-    //           body: data,
-    //     })
-    //     .then(console.log('success') )
-    //     .catch(
-    //         (err) => console.error(`Error golpeando el webhook ${err}`));
-         if (Object.keys(datos).length > 0) {
-          res.send(datos)
-         } else {
-          res.send('make a request');
-         }
-         
-  ;
+  await Cambio.find()
+   .then(cambio => {
+    console.log(cambio);
+    axios.post('https://webhook.site/04a014a0-f138-4869-b665-a3ed188cdba5', {
+  body: JSON.stringify(cambio),
+});
+    res.json(cambio)
+  }).then()
+  .catch(err => res.status(404).json({ sinDatos: 'No existen datos, corra data.js' }));
+  
+  
 });
 
 
